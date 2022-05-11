168bdbf99cd036e7d834d7e5fed317db56be9bf6
Kevin Rogers <kevinarogers1@gmail.com>
2022-05-10 22:28:48 -0400

    commit tree 948b8cece7a1095fd23d78b4c5f1857c91966baa

--- src/components/App.js
+++ src/components/App.js
@@ -5,47 +5,44 @@ import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
-// console.log({ TASKS });
-// console.log({ CATEGORIES });
 function App() {
   const [tasks, setTasks] = useState(TASKS);
   const [filterCategory, setFilterCategory] = useState("All");
 
   function handleDelete(text) {
-    const filteredTasks = tasks.filter((task) => task.text !== text)
+    const filteredTasks = tasks.filter((task) => task.text !== text);
     setTasks(filteredTasks);
-  } 
-
-  const handleClick = (e) => {
-    setFilterCategory(e.target.innerText);
-    // e.targe.className='selected';
   }
 
   const displayTasks = () => {
-    return tasks.filter(task => {
-      if(filterCategory === "All") {
-        return true
+    return tasks.filter((task) => {
+      if (filterCategory === "All") {
+        return true;
       } else {
-        return filterCategory === task.category
+        return filterCategory === task.category;
       }
-    })
-  }
-
+    });
+  };
 
   const handleFormSubmit = (newTask) => {
-    setTasks([...tasks, newTask ])
-  } 
-
+    setTasks([...tasks, newTask]);
+  };
 
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter categories={CATEGORIES} handleClick={handleClick} filterCategory={filterCategory} />
-      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleFormSubmit} />
+      <CategoryFilter
+        categories={CATEGORIES}
+        handleClick={setFilterCategory}
+        filterCategory={filterCategory}
+      />
+      <NewTaskForm
+        categories={CATEGORIES}
+        onTaskFormSubmit={handleFormSubmit}
+      />
       <TaskList tasks={displayTasks()} handleDelete={handleDelete} />
     </div>
   );
 }
 
 export default App;
-
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,18 +1,15 @@
 import React from "react";
 
-// When the delete button is clicked, the task should be removed from the list.
-function Task({ task, handleDelete }) {
-  
-  
+function Task({ text, category, handleDelete }) {
   return (
     <div className="task">
-      <div className="label">{task.category}</div>
-      <div className="task">{task.text}</div>
-      <button className="delete" onClick={() => handleDelete(task.text)}>X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={() => handleDelete(text)}>
+        X
+      </button>
     </div>
   );
 }
 
 export default Task;
-
-
