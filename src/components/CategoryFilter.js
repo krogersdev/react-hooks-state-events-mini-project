5df666705fac8c85d52ba656478617e340233436
Kevin Rogers <kevinarogers1@gmail.com>
2022-05-10 22:28:51 -0400

    Completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,29 +1,24 @@
 import React from "react";
-import { v4 as uuidv4 } from "uuid";
 
 function CategoryFilter({ categories, handleClick, filterCategory }) {
-  //
-  const renderButtons = () => {
-    return categories.map((category) => {
-      return (
-        <button
-          className={category === filterCategory ? "selected" : null}
-          onClick={handleClick}
-          key={uuidv4()}
-        >
-          {category}
-        </button>
-      );
-    });
-  };
+  const renderButtons = categories.map((category) => {
+    return (
+      <button
+        className={category === filterCategory ? "selected" : null}
+        onClick={() => handleClick(category)}
+        key={category}
+      >
+        {category}
+      </button>
+    );
+  });
 
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
-      {renderButtons()}
+      {renderButtons}
     </div>
   );
 }
-
 export default CategoryFilter;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,23 +1,20 @@
 import React from "react";
 import Task from "./Task";
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 function TaskList({ tasks, handleDelete }) {
-
   const displayTaskList = tasks.map((task) => {
-    return (<Task key={uuidv4()} task={task} handleDelete={handleDelete} />)
-  })
+    return (
+      <Task
+        key={uuidv4()}
+        text={task.text}
+        category={task.category}
+        handleDelete={handleDelete}
+      />
+    );
+  });
 
-  return (
-    <div className="tasks">
-      {displayTaskList}
-    </div>
-  );
+  return <div className="tasks">{displayTaskList}</div>;
 }
 
 export default TaskList;
-
-
-// displaying a list of tasks using Task component 
-
